6ddceee6e2e83c726675045bc2d12de4a765cb04
Brayan Duque <bduque@lean-tech.io>
2024-07-04 10:59:04 -0500

    feat(add events): emit event

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,6 +1,7 @@
 <script setup>
     import Question from "../components/Question.vue";
     import QuizHeader from "../components/QuizHeader.vue";
+    import Result from "../components/Result.vue";
     import { useRoute } from 'vue-router'
     import { ref, computed } from 'vue';
     import quizes from '../data/quizes.json';
@@ -9,9 +10,22 @@
     const quizId = parseInt(route.params.id);
     const quiz = quizes.find(quiz => quiz.id === quizId);
     const currentQuestionIndex = ref(0);
+    const numberOfCorrectAnswers = ref(0);
+    const showResults = ref(false);
 
     const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
     const barPercentage = computed(() => `${currentQuestionIndex.value / quiz.questions.length * 100}%`)
+
+    const onOptionSelected = (isCorrect) => {
+        if(isCorrect) {
+            numberOfCorrectAnswers.value++;
+        }
+
+        if(quiz.questions.length -1 === currentQuestionIndex.value) {
+            showResults.value = true;
+        }
+        currentQuestionIndex.value++;
+    }
 </script>
 
 <template>
@@ -21,7 +35,15 @@
             :barPercentage="barPercentage"
         />
         <div>
-            <Question :question="quiz.questions[currentQuestionIndex]"/>
+            <Question 
+            v-if="!showResults"
+            :question="quiz.questions[currentQuestionIndex]"
+            @selectOption="onOptionSelected"
+            />
+            <Result v-else
+            :quizQuestionLength="quiz.questions.length"
+            :numberOfCorrectAnswers="numberOfCorrectAnswers"
+            />
         </div>
     </div>
 </template>
